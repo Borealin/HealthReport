fcdeb46710ff6ad35a4e27b235545984313b2fa1
shichuning_boling <shichuning.scn@alibaba-inc.com>
2021-08-06 14:22:19 +0800

    [FIX] remove dependencies of rsa bs4 and lxml

--- passport.py
+++ passport.py
@@ -1,12 +1,40 @@
 import requests
 import json
-from rsa_no_padding import Encrypt
-from bs4 import BeautifulSoup
+import re
 
 header = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
 }
 
+class Encrypt:
+    def __init__(self, e, m):
+        self.e = e
+        self.m = m
+
+    def encrypt(self, message: str) -> str:
+        mm = int(self.m, 16)
+        ee = int(self.e, 16)
+        crypto = self._encrypt(message.encode(), mm, ee)
+        return crypto.hex()
+
+    def _pad_for_encryption(self, message: str, target_length: int) -> bytes:
+        return b'\x00'*(target_length - len(message)) + message
+
+    def _encrypt(self, message, mm: int, ee: int) -> bytes:
+        keylength = self.byte_size(mm)
+        padded = self._pad_for_encryption(message, keylength)
+        payload = int.from_bytes(padded, 'big', signed=False)
+        encrypted = pow(payload, ee, mm)
+        block = encrypted.to_bytes(keylength, 'big')
+        return block
+
+    def byte_size(self, number: int) -> int:
+        if number == 0:
+            return 1
+        quanta, mod = divmod(number.bit_length(), 8)
+        if mod:
+            quanta += 1
+        return quanta
 
 class LoginFailedException(Exception):
     def __init__(self, err='wrong user name or password'):
@@ -35,9 +63,7 @@ class User:
         except Exception as e:
             page_text = self.session.get(
                 self.fallback_base_url + self.login_url, headers=header, verify=False).text
-        soup = BeautifulSoup(page_text, 'lxml')
-        execution_id = soup.findAll(
-            'input', attrs={'name': 'execution'})[0]['value']
+        execution_id = re.findall(r'<[\s\n ]*input[\s\n ]*type[\s\n ]*=[\s\n ]*"hidden"[\s\n ]*name[\s\n ]*=[\s\n ]*"execution"[\s\n ]*value[\s\n ]*=[\s\n ]*"(.*?)"[\s\n ]*/>', page_text)[0]
         enc_user_pass = self.pass_encrypt(self.user_pass)
         form = {'username': self.user_id,
                 'password': enc_user_pass,
